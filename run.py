c41d774adc32911b01b21e823a53fc620ae3bc39
dmitryimalkov <59419740+dmitryimalkov@users.noreply.github.com>
2021-03-17 19:11:02 +0300

    Add files via upload

--- run.py
+++ run.py
@@ -2,10 +2,21 @@
 
 from fastapi import FastAPI
 from models.user import User
+from models.book import Book
+from models.author import Author
+
 app = FastAPI()
 
 #For LogIn System
 @app.post("/user")
 async def post_user(user: User):
     return {"Request body": user}
+# when you forward parameters under get method no need to add ?: /user?param
+@app.get("/user")
+async def get_user_validation(password: str):
+    return {"Query param": password}
 
+# when you take variable parameter you should put it in {}
+@app.get("/book/{isbn}")
+async def get_book_with_isbn(isbn: str):
+    return {"Query isbn var": isbn}
